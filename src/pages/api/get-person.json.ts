6e0236dba6774d033e1487868524ef2ff2eb2796
Lenar Lima <lenar.toledo@gmail.com>
2024-08-26 23:02:30 -0400

    CRUD using Astro

---
+++ src/pages/api/get-person.json.ts
@@ -0,0 +1,18 @@
+
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async ({ params, request }) => {
+
+
+    const person = {
+        name: 'Lenar Toledo',
+        age: 31
+    }
+
+    return new Response(JSON.stringify(person), { 
+        status: 200, 
+        headers: {
+            'Content-type': 'application/json'
+        }  
+    });
+};
